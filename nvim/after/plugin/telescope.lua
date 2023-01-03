08f23ed32e1461fa51e70dfea3fb67b2ae4b2304
Thomas Chow <tomwf.chow@gmail.com>
2023-01-03 22:53:00 +0100

    nvim: remap help tags

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,7 +3,7 @@ vim.keymap.set('n', '<leader>fh', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>fo', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fg', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fd', builtin.help_tags, {})
 
 local actions = require('telescope.actions')
 require('telescope').setup {
