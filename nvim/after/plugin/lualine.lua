33e0e4feb4830f430ef5f6dcde8dab388a3be3ae
Thomas Chow <tomwf.chow@gmail.com>
2023-03-04 18:45:48 +0100

    nvim: add lualine bubble theme

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = "|",
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
@@ -18,20 +18,24 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_a = { "mode" },
+		lualine_a = {
+			{ "mode", separator = { left = "" }, right_padding = 2 },
+		},
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
-		lualine_z = { "location" },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
 	},
 	inactive_sections = {
-		lualine_a = {},
+		lualine_a = { "filename" },
 		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
+		lualine_c = {},
+		lualine_x = {},
 		lualine_y = {},
-		lualine_z = {},
+		lualine_z = { "location" },
 	},
 	tabline = {},
 	winbar = {},
