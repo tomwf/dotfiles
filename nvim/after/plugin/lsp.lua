22bfd43459d3207197bf60667a13ecb2c975477f
Thomas Chow <tomwf.chow@gmail.com>
2023-09-30 22:40:17 +0200

    nvim: add lsp mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -3,15 +3,11 @@ local lsp = require("lsp-zero")
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 	local bind = vim.keymap.set
+  lsp.default_keymaps({buffer = bufnr})
 
-	bind("n", "gd", vim.lsp.buf.definition, opts)
-	bind("n", "K", vim.lsp.buf.hover, opts)
-	bind("n", "<leader>z", vim.lsp.buf.type_definition, opts)
-	bind("n", "<leader>r", vim.lsp.buf.rename, opts)
-	bind("n", "gr", vim.lsp.buf.references, opts)
-	bind("n", "<leader>lf", function()
-		vim.lsp.buf.format({ async = true })
-	end, opts)
+	bind("n", "<leader>rn", vim.lsp.buf.rename, opts)
+	bind("n", "<leader>rf", vim.lsp.buf.format, opts)
+	bind("n", "<leader>ra", vim.lsp.buf.code_action, opts)
 end)
 
 require('mason').setup({})
