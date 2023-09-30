b6175da5eeb7ee6e10a2de6b9adf9288a5ef34b3
Thomas Chow <tomwf.chow@gmail.com>
2023-09-30 22:17:47 +0200

    nvim: replace packer with lazy

--- nvim/lua/tomwf/plugins.lua
+++ nvim/lua/tomwf/plugins.lua
@@ -1,90 +1,87 @@
--- Only required if you have packer configured as `opt`
-vim.cmd([[packadd packer.nvim]])
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 
-return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	-- Syntax Highlight
-	use({ "catppuccin/nvim", as = "catppuccin" })
-	-- Beautiful Statusline
-	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "nvim-tree/nvim-web-devicons" })
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
-	-- Superpowers
-	use("tpope/vim-surround")
-	use("tpope/vim-commentary")
-	use("tpope/vim-repeat")
-	-- Git magic
-	use("tpope/vim-fugitive")
-	use("airblade/vim-gitgutter")
-	use("APZelos/blamer.nvim")
-	-- Coc is love
-	use({ "neoclide/coc.nvim", branch = "release" })
-	-- Time Tracking
-	use("wakatime/vim-wakatime")
-	-- Tree sitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	})
-	-- LSP + autocompletion + snippets
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "hrsh7th/cmp-cmdline" },
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
-	-- Linters & formatters
-	use("jose-elias-alvarez/null-ls.nvim")
-	-- Telescope
-	use({
-		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	-- AI autocompletion
-	use({
-		"Exafunction/codeium.vim",
-		config = function()
-			vim.keymap.set("i", "<C-f>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-j>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-k>", function()
-				return vim.fn["codeium#CycleCompletions"](-1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
-		end,
-	})
-	-- undotree
-	use({
-		"jiaoshijie/undotree",
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
-end)
+require("lazy").setup({
+  -- Syntax Highlight
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- Beautiful Statusline
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  -- Superpowers
+  {"tpope/vim-surround"},
+  {"tpope/vim-commentary"},
+  {"tpope/vim-repeat"},
+  -- Git magic
+  {"tpope/vim-fugitive"},
+  {"airblade/vim-gitgutter"},
+  {"APZelos/blamer.nvim"},
+  -- Coc is love
+  { "neoclide/coc.nvim", branch = "release" },
+  -- Time Tracking
+  {"wakatime/vim-wakatime"},
+  -- Tree sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+  },
+  -- LSP + autocompletion + snippets
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  -- Autocompletion
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-nvim-lua" },
+  { "hrsh7th/cmp-cmdline" },
+  -- Snippets
+  { "L3MON4D3/LuaSnip" },
+  { "rafamadriz/friendly-snippets" },
+  -- Linters & formatters
+  {"jose-elias-alvarez/null-ls.nvim"},
+  -- Telescope
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
+  -- AI autocompletion
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      vim.keymap.set("i", "<C-f>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<C-j>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-k>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<C-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+})
