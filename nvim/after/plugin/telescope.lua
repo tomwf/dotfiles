242f5b12bf45fca1e73f24e3560d7c6b9e8ba949
Thomas Chow <thomas.chow@polyconseil.fr>
2024-06-27 23:25:13 +0200

    nvim: add telescope mappings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -12,7 +12,9 @@ require("telescope").setup({
 			i = {
 				["<C-j>"] = actions.move_selection_next,
 				["<C-k>"] = actions.move_selection_previous,
-				["<C-f>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-v>"] = actions.send_selected_to_qflist + actions.open_qflist,
+				["<C-f>"] = actions.results_scrolling_down,
+				["<C-b>"] = actions.results_scrolling_up,
 			},
 		},
 	},
