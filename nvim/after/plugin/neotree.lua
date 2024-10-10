47e91a3d361a7f886154d5c99d82542aeb45844f
Thomas Chow <kirthmas@mac-1.home>
2024-10-10 10:55:21 +0200

    nvim: format code

--- nvim/after/plugin/neotree.lua
+++ nvim/after/plugin/neotree.lua
@@ -1,10 +1,10 @@
 require("neo-tree").setup({
-  popup_border_style = "rounded",
-  window = {
-    mappings = {
-      ["l"] = "open",
-      ["L"] = "focus_preview",
-      ["h"] = "close_node",
-    }
-  },
+	popup_border_style = "rounded",
+	window = {
+		mappings = {
+			["l"] = "open",
+			["L"] = "focus_preview",
+			["h"] = "close_node",
+		},
+	},
 })
