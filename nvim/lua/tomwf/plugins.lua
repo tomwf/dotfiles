56951d0a2c236b27ccf3e243a79f8833e787a2be
Thomas Chow <tomwf.chow@gmail.com>
2024-11-12 16:47:56 +0100

    nvim: add dapui plugin

--- nvim/lua/tomwf/plugins.lua
+++ nvim/lua/tomwf/plugins.lua
@@ -102,4 +102,5 @@ require("lazy").setup({
 		"mfussenegger/nvim-dap",
 		"jay-babu/mason-nvim-dap.nvim",
 	},
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
 })
