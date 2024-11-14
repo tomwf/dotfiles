4b3416a177d1de282d86bde2d059f7e7c0cb9f05
Thomas Chow <tomwf.chow@gmail.com>
2024-11-14 15:28:45 +0100

    nvim: add telescope mappings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,21 +1,25 @@
 local builtin = require("telescope.builtin")
 -- Files
-vim.keymap.set("n", "<leader>fh", builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set("n", "<leader>fo", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
 vim.keymap.set("n", "<leader>ff", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+-- Vim
+vim.keymap.set("n", "<leader>fh", builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>f:", builtin.command_history, {})
+vim.keymap.set("n", "<leader>f/", builtin.search_history, {})
 vim.keymap.set("n", '<leader>f"', builtin.registers, {})
 -- LSP
-vim.keymap.set("n", "<leader>lr", builtin.lsp_references, {})
-vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, {})
-vim.keymap.set("n", "<leader>la", builtin.lsp_workspace_symbols, {})
-vim.keymap.set("n", "<leader>ld", builtin.diagnostics, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>fd", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>fw", builtin.diagnostics, {})
 -- Git
 vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
 vim.keymap.set("n", "<leader>gl", builtin.git_commits, {})
 vim.keymap.set("n", "<leader>gf", builtin.git_bcommits, {})
+-- Treesitter
+vim.keymap.set("n", "<leader>ft", builtin.treesitter, {})
 -- Emoji
 vim.keymap.set("n", "<leader>fe", "<Cmd>Telescope emoji<CR>", {})
 
