b6fdb88cdc316339ce7ff102f409546a8bf330ef
Thomas Chow <tomwf.chow@gmail.com>
2025-06-16 17:13:00 +0200

    nvim: add flash plugin

--- nvim/lua/tomwf/plugins.lua
+++ nvim/lua/tomwf/plugins.lua
@@ -31,6 +31,20 @@ require("lazy").setup({
 	{ "tpope/vim-surround" },
 	{ "tpope/vim-commentary" },
 	{ "tpope/vim-repeat" },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+	},
 	-- Git magic
 	{ "tpope/vim-fugitive" },
 	{ "lewis6991/gitsigns.nvim" },
